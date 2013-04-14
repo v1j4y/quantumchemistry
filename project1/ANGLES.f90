084097b1485734d3c63d54e615d1ea5a1c27529f
vijaygopalchilkuri <vijay.gopal.c@gmail.com>
2013-04-14 23:59:00 +0200

    common not working

--- project1/ANGLES.f90
+++ project1/ANGLES.f90
@@ -1,10 +1,10 @@
-    SUBROUTINE ANGLES
+SUBROUTINE ANGLES
 
         PARAMETER(natomax=20)
-        double precision &
-        ex(natomax,natomax),ey(natomax,natomax),ez(natomax,natomax)
+        integer i,j
+        double precision ex(natomax,natomax),ey(natomax,natomax),ez(natomax,natomax)
         double precision ddist(natomax,natomax)
-        double precision angle(natomax,natomax)
+        double precision theta(natomax,natomax,natomax)
         logical :: yw
 
         common/coord/atyp(natomax),atoms,xx(natomax),yy(natomax),zz(natomax)
@@ -13,15 +13,27 @@
 
         ddist=0
 
+! ============================================================
+! calculating all possible angles
+! ============================================================
+    if(yw)then
+        write(6,*)'In ANGLES'
+    endif
+
     do j=1,atoms
         do i=j,atoms
-            ddist(j,i)=sqrt( ((xx(i)-xx(j))**2) + ((yy(i)-yy(j))**2) &
-                        + ((zz(i)-zz(j))**2) )
-            ex(j,i)= -(xx(j)-xx(i))/ddist(j,i)
-            ey(j,i)= -(yy(j)-yy(i))/ddist(j,i)
-            ez(j,i)= -(zz(j)-zz(i))/ddist(j,i)
+
+!           ddist(j,i)=sqrt( ((xx(i)-xx(j))**2) + ((yy(i)-yy(j))**2) &
+!                       + ((zz(i)-zz(j))**2) )
+
+            if(ddist(j,i).ne.0.d0)then
+                ex(j,i)= -(xx(j)-xx(i))/ddist(j,i)
+                ey(j,i)= -(yy(j)-yy(i))/ddist(j,i)
+                ez(j,i)= -(zz(j)-zz(i))/ddist(j,i)
+            endif
 
             if(yw)then
+                write(6,*)'test'
                 write(6,1000)i,j,ddist(j,i)
             endif
         enddo
@@ -29,15 +41,20 @@
 
     do j=1,atoms
         do i=1,atoms
-            if(ddist(j,i).ne.0.d0)then
-                ex(j,i)= -(xx(j)-xx(i))/ddist(j,i)
-                ey(j,i)= -(yy(j)-yy(i))/ddist(j,i)
-                ez(j,i)= -(zz(j)-zz(i))/ddist(j,i)
-            endif
+            do k=1,atoms
+
+                theta(j,i,k)=ex(j,i)*ex(i,k)+ey(j,i)*ey(i,k)+ez(j,i)*ez(i,k)
+
+                if(yw)then
+                     write(6,1001)k,i,j,theta(j,i,k)
+                endif
+
+            enddo
         enddo
     enddo
 
 
 1000 FORMAT(3x,('R(',I2,',',I2,') = ',F12.4))
+1001 FORMAT(3x,('R(',I2,',',I2,',',I3,') = ',F12.4))
 
     end
--- project1/DIST.f90
+++ project1/DIST.f90
@@ -2,6 +2,7 @@
     PARAMETER(natomax=20)
 
     logical :: yw
+    integer i,j
 !   integer atoms
 !   double precision xx(natomax),yy(natomax),zz(natomax)
 !   double precision atyp(natomax)
@@ -13,14 +14,21 @@
 
 
 ! ============================================================
-! reading inputs from .inp
+! calculating distances R_ij
 ! ============================================================
+    if(yw)then
+        write(6,*)'In DIST'
+        write(6,*)atoms
+        write(6,*)xx(2)
+    endif
 
     do j=1,atoms
         do i=j,atoms
+
             ddist(j,i)=sqrt( ((xx(i)-xx(j))**2) + ((yy(i)-yy(j))**2) &
                         + ((zz(i)-zz(j))**2) )
             if(yw)then
+                write(6,*)'test'
                 write(6,1000)i,j,ddist(j,i)
             endif
         enddo
